8f83c7313d042dadb564953ebad3103e0dbe020c
shilohbradley <shilohbradley@yahoo.com>
2020-07-23 20:46:08 -0700

    chapter 13 initial commit

---
+++ Chapter13.R
@@ -0,0 +1,12 @@
+## Install packages -----
+install.packages(c("ez", "multcomp", "nlme", "pastecs", "reshape"))
+install.packages("WRS", repos = "http://R-Forge.R-project.org")
+
+## Load packages -----
+library(ez)
+library(ggplot2)
+library(multcomp)
+library(nlme)
+library(pastecs)
+library(reshape)
+library(WRS)
